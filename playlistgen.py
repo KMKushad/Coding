2cb2308ab07a348e1935bf2cd65c41f79731b1eb
KMKushad <kmkushad@gmail.com>
2023-02-22 21:38:20 -0500

    made playlistgen

--- playlistgen.py
+++ playlistgen.py
@@ -1,61 +1,76 @@
-import google.auth
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
 import os
-import google.auth.transport.requests
-import google.oauth2.credentials
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+import time
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\manik\Downloads\playlist-generator-378301-2f08825762f9.json"
-# Set up the API client
-#credentials, project = google.auth.default()
-#api_key = 'AIzaSyCoJCdcyut7evw2jEqjzfCWR2Sbrw1HzwQ'
-#youtube = build('youtube', 'v3', credentials=credentials)
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
 
-credentials, project = google.auth.default(scopes=['https://www.googleapis.com/auth/youtube.force-ssl'])
-if credentials is None or credentials.expired:
-    credentials, _ = google.auth.default(scopes=['https://www.googleapis.com/auth/youtube.force-ssl'])
-    request = google.auth.transport.requests.Request()
-    credentials.refresh(request)
+scopes = ["https://www.googleapis.com/auth/youtube.readonly", "https://www.googleapis.com/auth/youtube.force-ssl"]
 
-youtube = build('youtube', 'v3', credentials=credentials)  
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-# Define the playlist parameters
-playlist_params = {
-    'snippet': {
-        'title': 'My Awesome Playlist',
-        'description': 'A playlist of programming tutorials.'
-    },
-    'status': {
-        'privacyStatus': 'public'
-    }
-}
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = r"C:\Users\manik\Downloads\client_secret_72424185993-r3f4e2nc2sfehrp288sk5cmu4rskji6m.apps.googleusercontent.com.json"
 
-# Create the playlist
-playlist_response = youtube.playlists().insert(
-    part='snippet,status',
-    body=playlist_params
-).execute()
+# Get credentials and create an API client
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_console()
+youtube = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
 
-playlist_id = playlist_response['id']
-print(f'Playlist created with ID: {playlist_id}')
-
-# Add videos to the playlist
-video_ids = ['VIDEO_ID_1', 'VIDEO_ID_2', 'VIDEO_ID_3']
-
-for video_id in video_ids:
-    playlist_item_params = {
-        'snippet': {
-            'playlistId': playlist_id,
-            'resourceId': {
-                'kind': 'youtube#video',
-                'videoId': video_id
+def addToPlaylist(video_id):
+    request = youtube.playlistItems().insert(
+        part="snippet",
+        body={
+          "snippet": {
+            "playlistId": "PLPE3jYWp3YNKZZ0Ry4W-Z02SBC3ulY-kF",
+            "resourceId": {
+              "kind": "youtube#video",
+              "videoId": video_id
             }
+          }
         }
-    }
+    )
+    response = request.execute()
 
-    playlist_item_response = youtube.playlistItems().insert(
-        part='snippet',
-        body=playlist_item_params
-    ).execute()
+    print(response)
 
-    print(f'Added video with ID {video_id} to playlist with ID {playlist_id}')
+def search(topic):
+    request = youtube.search().list(
+        part="snippet",
+        maxResults=1,
+        q=topic,
+        type="youtube#video",
+        order='relevance'
+    )
+    response = request.execute()
+
+    id = response['items'][0]['id']['videoId']
+
+    return id
+
+def main():
+
+    input = open("input.txt", "r")
+
+    for line in input.readlines():
+        print(line)
+        id = search(line)
+        addToPlaylist(id)
+        time.sleep(0.5)
+    
+    input.close()
+
+if __name__ == "__main__":
+    main()
+
